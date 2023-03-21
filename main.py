790bf3207f0b78e1379352225215373db0005e05
DashaMax <be9emotkot@yandex.ru>
2023-03-21 13:03:21 +0300

    update main.py

--- main.py
+++ main.py
@@ -3017,12 +3017,114 @@ import random
 
 
 
+#################
+#  Дескрипторы  #
+#################
+
+
+# class ValidString:
+#     def __set_name__(self, owner, name):
+#         self.__name = name
+#
+#     def __get__(self, instance, owner):
+#         return instance.__dict__[self.__name]
+#
+#     def __set__(self, instance, value):
+#         if not isinstance(value, str):
+#             raise ValueError(f'{self.__name} должно быть строкой')
+#         instance.__dict__[self.__name] = value
+#
+#
+# class Person:
+#     name = ValidString()
+#     surname = ValidString()
+#
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#
+# person = Person('Ivan', 'Petrov')
+# print(person.name)
 
 
 
 
 
 
+
+############################
+#  Дескриптор class Order  #
+############################
+
+
+# class NoNegative:
+#     def __set_name__(self, owner, name):
+#         self.name = name
+#
+#     def __get__(self, instance, owner):
+#         return instance.__dict__[self.name]
+#
+#     def __set__(self, instance, value):
+#         if not isinstance(value, int) or value <= 0:
+#             raise ValueError('Значение должно быть положительным')
+#         instance.__dict__[self.name] = value
+#
+#
+# class Order:
+#     price = NoNegative()
+#     quantity = NoNegative()
+#
+#     def __init__(self, name, price, quantity):
+#         self.order = name
+#         self.price = price
+#         self.quantity = quantity
+#
+#     def get_total(self):
+#         return self.price * self.quantity
+#
+#
+# order = Order('apple', 5, 10)
+# print(order.get_total())
+
+
+
+
+
+
+
+
+##############################
+#  Дескриптор class Point3D  #
+##############################
+
+
+# class Integer:
+#     def __set_name__(self, owner, name):
+#         self.name = '_' + name
+#
+#     def __get__(self, instance, owner):
+#         return getattr(instance, self.name)
+#
+#     def __set__(self, instance, value):
+#         if not isinstance(value, int):
+#             raise TypeError(f'Координата {self.name} должна быть целым числом')
+#         instance.__dict__[self.name] = value
+#
+#
+# class Point3D:
+#     x = Integer()
+#     y = Integer()
+#     z = Integer()
+#
+#     def __init__(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#
+# point = Point3D(1, 2, 3)
+# print(point.__dict__)
 
 
 
