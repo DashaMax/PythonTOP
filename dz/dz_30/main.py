592ccb403f9d45723a837e5b73d7350556b0e1fc
DashaMax <be9emotkot@yandex.ru>
2023-03-25 17:19:38 +0300

    add person.json

---
+++ dz/dz_30/main.py
@@ -0,0 +1,36 @@
+from json import load, dump
+from random import randint, choice
+from string import ascii_lowercase, digits
+
+
+def get_person() -> dict:
+    name = ''.join(choice(ascii_lowercase) for _ in range(randint(3, 7))).capitalize()
+    telephone = '+79' + ''.join(choice(digits) for _ in range(9))
+    person = {
+        'name': name,
+        'telephone': telephone
+    }
+    return person
+
+
+def write_person_json(person: dict):
+    try:
+        with open('dz/dz_30/person.json') as file:
+            data = load(file)
+
+    except FileNotFoundError:
+        data = {}
+
+    ids = list(data.keys())
+
+    if ids:
+        data[int(ids[-1]) + 1] = person
+    else:
+        data[1] = person
+
+    with open('dz/dz_30/person.json', 'w') as file:
+        dump(data, file, indent=2)
+
+
+for _ in range(10):
+    write_person_json(get_person())
