7e4ef1ed80af8a71f073a12d03a65dcb4da98ec8
DashaMax <be9emotkot@yandex.ru>
2023-04-02 02:03:28 +0300

    parse chitai-gorod

--- dz/dz_32/main.py
+++ dz/dz_32/main.py
@@ -14,53 +14,55 @@ def get_html(url: str) -> str:
 
     if response.status_code == 200:
         return response.text
-    raise ConnectionError
+
+    raise ConnectionError('Ошибка подключения')
 
 
 def get_src(a: str) -> str:
-    find_src = re.findall(r'data-src="(.+)" data-srcset=', a)
-    find_src = find_src[0] if find_src else ''
-    return find_src
+    src = re.findall(r'data-src="(.+)" data-srcset=', a)
+    src = src[0] if src else ''
+    return src
 
 
 def write_product(product: list):
     try:
-        with open('books.csv') as file:
+        with open('book.csv') as file:
             pass
 
     except FileNotFoundError:
-        with open('books.csv', 'w') as file:
+        with open('book.csv', 'w') as file:
             writer = csv.writer(file, delimiter=';', lineterminator='\r')
             writer.writerow(['Название', 'Автор', 'Цена', 'URL изображения'])
 
-    with open('books.csv', 'a') as file:
+    with open('book.csv', 'a') as file:
         writer = csv.writer(file, delimiter=';', lineterminator='\r')
         writer.writerow(product)
 
 
-def parse_data(html: str):
+def get_product(html: str):
     soup = BeautifulSoup(html, 'html.parser')
     product_list = soup.find_all('article', class_='product-card')
-
-    for product in product_list:
-        title = str(product.find('div', class_='product-title__head').text).strip()
-        author = str(product.find('div', class_='product-title__author').text).strip()
-        price = str(product.find('div', class_='product-price__value').text).strip()
-        img = get_src(str(product.find('img', class_='product-picture__img')))
-        write_product([title, author, price, img])
+    return product_list
 
 
 def main():
     pages = 21   # на сайте 2192 страницы
 
-    for i in range(1, pages):
-        if i == 1:
+    for page in range(1, pages):
+        if page == 1:
             url = 'https://www.chitai-gorod.ru/catalog/books/hudozhestvennaya-literatura-9657'
         else:
-            url = 'https://www.chitai-gorod.ru/catalog/books/hudozhestvennaya-literatura-9657?page=' + str(i)
+            url = 'https://www.chitai-gorod.ru/catalog/books/hudozhestvennaya-literatura-9657?page=' + str(page)
 
         html = get_html(url)
-        parse_data(html)
+        books = get_product(html)
+
+        for book in books:
+            title = str(book.find('div', class_='product-title__head').text).strip()
+            author = str(book.find('div', class_='product-title__author').text).strip()
+            price = str(book.find('div', class_='product-price__value').text).strip()
+            img = get_src(str(book.find('img', class_='product-picture__img')))
+            write_product([title, author, price, img])
 
 
 if __name__ == '__main__':
