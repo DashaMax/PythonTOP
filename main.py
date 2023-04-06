3832b09d61894222bc4054b834a4f3cc98e0df4c
DashaMax <be9emotkot@yandex.ru>
2023-04-06 13:02:58 +0300

    update main.py and MVC articles

--- main.py
+++ main.py
@@ -3412,6 +3412,29 @@ import random
 
 
 
+############
+#  socket  #
+############
+
+
+import socket
+
+
+def run():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 5000))
+    server_socket.listen()
+
+    while True:
+        client_socket, address = server_socket.accept()
+        request = client_socket.recv(1024)
+
+        print(f'Клиент: {address} => \n{request}\n')
+
+
+if __name__ == '__main__':
+    run()
+
 
 
 
